709256f38a698853110808b89954d16bd7159821
Zvezdelin Penkov <115587735+Zvezdi2003@users.noreply.github.com>
2023-06-16 21:16:35 +0300

    Update CommandPrompt.cpp

--- CommandPrompt.cpp
+++ CommandPrompt.cpp
@@ -1,166 +1,160 @@
 #include "CommandPrompt.h"
 
 
-bool CommandPrompt::registerUser(const MyString& firstName,
-	const MyString& lastName,
-	const MyString& password,
-	unsigned id,
-	double points)
+void CommandPrompt::logout()
 {
-	if (points < 0)
-		return false;
-
-	User user(firstName, lastName, password, 0);
-	users.pushBack(user);
-	return true;
+	isLoggedIn = false;
 }
-
-bool CommandPrompt::login(const MyString& firstName, const MyString& pass)
+void CommandPrompt::create(const MyString& topicTitle, const User& creator, const MyString& description)
 {
-	for (size_t i = 0; i < users.getSize(); i++)
-	{
-		if (users[i].getFirstName() == firstName && users[i].getPassword() == pass)
-		{
-			isLoggedIn = true;
-			currentUser = users[i];
-			return true;
-		}
-	}
-	return false;
+	Topic topic(topicTitle, creator, description);
+	topics.pushBack(topic);
+	std::cout << "> Success!";
 }
-
 void CommandPrompt::search(const MyString& text) const
 {
 	for (size_t i = 0; i < topics.getSize(); i++)
 	{
 		if (contains(topics[i].getTitle(), text))
 		{
-			std::cout << ">" << topics[i].getTitle() << topics[i].getId() << std::endl;
+			std::cout << " >> " << topics[i].getTitle() << " {id: " << topics[i].getId() << "}" << std::endl;
 		}
 	}
 }
 
-bool CommandPrompt::open(const MyString& title) 
+bool CommandPrompt::open(const MyString& title)
 {
 	for (size_t i = 0; i < topics.getSize(); i++)
 	{
 		if (topics[i].getTitle() == title)
 		{
-			currentTopic = topics[i];
-			std::cout << ">" << topics[i].getTitle();
+			std::cout << ">" << "Welcome to " << "\"" << topics[i].getTitle() << "\"!";
+			openedTopic = i;
 			return true;
 		}
 	}
 	return false;
 }
 
-bool CommandPrompt::open(unsigned id) 
+bool CommandPrompt::open(unsigned id)
 {
-	for (size_t i = 0; i < topics.getSize(); i++)
-	{
-		if (topics[i].getId() == id)
-		{
-			currentTopic = topics[i];
-			std::cout << ">" << topics[i].getTitle();
-			return true;
-		}
+	if (topics.getSize() <= id) {
+		std::cout << "Topic index not found";
+		return false;
 	}
-	return false;
+	std::cout << ">" << "Welcome to " << "\"" << topics[id].getTitle() << "\"!";
+	openedTopic = id;
+	return true;
 }
 
 void CommandPrompt::post(const MyString& header, const MyString& content, unsigned id)
 {
-	Question question(header, content, id);
-	questions.pushBack(question);
+	if (openedTopic != -1)
+	{
+		Question question(header, content, topics.getSize());
+		topics[openedTopic].add(question);
+	}
 }
 
-bool CommandPrompt::p_open(const MyString& title) 
+bool CommandPrompt::p_open(const MyString& title)
 {
-	for (size_t i = 0; i < questions.getSize(); i++)
-	{
-		if (questions[i].getHeader() == title)
-		{
-			currentQuestion = questions[i];
-			std::cout << ">" << questions[i].getHeader();
-			return true;
-		}
+	if (openedTopic != -1) {
+		std::cout << topics[openedTopic].findQuestion(title);
+		return true;
 	}
 	return false;
 }
 
-bool CommandPrompt::p_open(unsigned id) 
+bool CommandPrompt::p_open(unsigned id)
 {
-	for (size_t i = 0; i < questions.getSize(); i++)
-	{
-		if (questions[i].getId() == id)
-		{
-			currentQuestion = questions[i];
-			std::cout << ">" << questions[i].getHeader();
-			return true;
-		}
+	if (openedTopic != -1) {
+		std::cout << topics[openedTopic].findQuestion(id);
+		return true;
 	}
 	return false;
 }
 
 void CommandPrompt::addComment(const MyString& authorName, const MyString& commentText, unsigned commentIndex)
 {
-	Comment comment(authorName, commentText, commentIndex);
-	comments.pushBack(comment);
+	if (openedQuestion != -1) {
+		Comment comment(authorName, commentText, topics[openedTopic][openedQuestion].getId());
+		topics[openedTopic][openedQuestion].addComment(comment);
+		std::cout << "> Posted";
+		
+	}
+	
 }
 
-void CommandPrompt::printComments(const MyString& comments) const
+void CommandPrompt::printComments() const
 {
-	for (size_t i = 0; i < comments.length(); i++)
+	if (openedQuestion != -1)
 	{
-		std::cout << ">" << comments[i] << std::endl;
+		topics[openedTopic][openedQuestion].printComments();
 	}
 }
 
 void CommandPrompt::reply(unsigned id)
 {
-
+	if (openedQuestion != -1)
+	{
+		MyString reply;
+		std::cin >> reply;
+		topics[openedTopic][openedQuestion][id].addReply(reply);
+		std::cout << "> Posted";
+	}
 }
 
-void CommandPrompt::upvote(unsigned id)
+void CommandPrompt::upvote(unsigned id, const User& user)
 {
-
+	if (topics[openedTopic][openedQuestion].getSize() <= id)
+	{
+		throw std::invalid_argument("Invalid id!");
+	}
+	topics[openedTopic][openedQuestion][id].upvote(user);
 }
 
-void CommandPrompt::downvote(unsigned id)
+void CommandPrompt::downvote(unsigned id, const User& user)
 {
+	if (topics[openedTopic][openedQuestion].getSize() <= id)
+	{
+		throw std::invalid_argument("Invalid id!");
+	}
+	topics[openedTopic][openedQuestion][id].downvote(user);
 }
 
-void CommandPrompt::p_close() const
+void CommandPrompt::p_close()
 {
+	if (openedQuestion == -1)
+	{
+		std::cout << "The post is already closed!" << std::endl;
+	}
 }
 
-void CommandPrompt::quit() const
+void CommandPrompt::quit()
 {
-
-}
-
-void CommandPrompt::exit() const
-{
-
+	if (openedTopic == -1)
+	{
+		std::cout << "The topic is already closed!" << std::endl;
+	}
 }
 
 void CommandPrompt::whoami() const
 {
-	for (size_t i = 0; i < users.getSize(); i++)
-	{
-		std::cout << ">" << users[i].getFirstName() << ", " << users[i].getPoints() << std::endl;
-	}
+	std::cout << " >> " << currentUser.getFirstName() << ", " << currentUser.getPoints() << " points." << '\n';
 }
 
 void CommandPrompt::about(unsigned id) const
 {
-	for (size_t i = 0; i < topics.getSize(); i++)
+	std::cout << " >> Name: " << topics[id].getTitle(); 
+	std::cout << " >> Description: " << topics[id].getDescription();
+	std::cout << " >> Created by: " << topics[id].getCreator().getFirstName() << " " << topics[id].getCreator().getLastName() << " {id: " << id << "}";
+	std::cout << " >> Questions asked: " << topics[id].getQuestionsCount();
+}
+
+void CommandPrompt::list()
+{
+	if (openedTopic != -1)
 	{
-		if (topics[i].getId() == id)
-		{
-			std::cout << ">" << topics[i].getTitle() << std::endl << topics[i].getDescription() << std::endl <<
-				topics[i].getCreator().getFirstName() << topics[i].getCreator().getId() << std::endl <<
-				topics[i].getQuestionsCount() << std::endl;
-		}
+		topics[openedTopic].printQuestions();
 	}
 }
