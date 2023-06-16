45ab68601f0d1021826c7b585f1fff2ab31c9a2e
Zvezdelin Penkov <115587735+Zvezdi2003@users.noreply.github.com>
2023-06-16 21:12:43 +0300

    Update SocialNetwork.cpp

--- SocialNetwork.cpp
+++ SocialNetwork.cpp
@@ -1,10 +1,31 @@
-#include <iostream>
-#include <string>
-#include "Topic.h"
-#include "User.h"
+#pragma once
+#include "SocialNetwork.h"
 
-int main()
+bool SocialNetwork::registerUser(const MyString& firstName,
+	const MyString& lastName,
+	const MyString& password,
+	unsigned id,
+	double points)
 {
+	if (points < 0)
+		return false;
 
+	User user(firstName, lastName, password, 0);
+	users.pushBack(user);
+	return true;
 }
 
+bool SocialNetwork::login(const MyString& firstName, const MyString& pass)
+{
+	for (size_t i = 0; i < users.getSize(); i++)
+	{
+		if (users[i].getFirstName() == firstName && users[i].getPassword() == pass)
+		{
+			
+		}
+	}
+	return false;
+}
+SocialNetwork::SocialNetwork() {
+
+}
