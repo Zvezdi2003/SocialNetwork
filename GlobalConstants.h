0a374c507b605e115b30124d3fb4ce251f204728
Zvezdelin Penkov <115587735+Zvezdi2003@users.noreply.github.com>
2023-06-16 21:14:25 +0300

    Update GlobalConstants.h

--- GlobalConstants.h
+++ GlobalConstants.h
@@ -1,3 +1,22 @@
 #pragma once
+#include <iostream>
+#include "MyString.h"
 
-const int buff_Size = 1024;
+const MyString& SIGNUP_COMMAND = "signup";
+const MyString& LOGIN_COMMAND = "login";
+const MyString& LOGOUT_COMMAND = "logout";
+const MyString& OPEN_COMMAND = "open";
+const MyString& POST_COMMAND = "post";
+const MyString& P_OPEN_COMMAND = "p_open";
+const MyString& COMMENT_COMMAND = "comment";
+const MyString& COMMENTS_COMMAND = "comments";
+const MyString& REPLY_COMMAND = "reply";
+const MyString& UPVOTE_COMMAND = "upvote";
+const MyString& DOWNVOTE_COMMAND = "downvote";
+const MyString& P_CLOSE_COMMAND = "p_close";
+const MyString& QUIT_COMMAND = "quit";
+const MyString& EXIT_COMMAND = "exit";
+const MyString& WHOAMI_COMMAND = "whoami";
+const MyString& ABOUT_COMMAND = "about";
+
+const int buffSize = 1024;
