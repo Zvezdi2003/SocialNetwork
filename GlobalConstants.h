e7db0760d5afb3cf7b17af203dba0e6b0643f2c4
Zvezdelin Penkov <115587735+Zvezdi2003@users.noreply.github.com>
2023-06-18 23:17:40 +0300

    Update GlobalConstants.h

--- GlobalConstants.h
+++ GlobalConstants.h
@@ -5,8 +5,11 @@
 const MyString& SIGNUP_COMMAND = "signup";
 const MyString& LOGIN_COMMAND = "login";
 const MyString& LOGOUT_COMMAND = "logout";
+const MyString& CREATE_COMMAND = "create";
+const MyString& SEARCH_COMMAND = "search";
 const MyString& OPEN_COMMAND = "open";
 const MyString& POST_COMMAND = "post";
+const MyString& LIST_COMMAND = "list";
 const MyString& P_OPEN_COMMAND = "p_open";
 const MyString& COMMENT_COMMAND = "comment";
 const MyString& COMMENTS_COMMAND = "comments";
@@ -18,5 +21,3 @@ const MyString& QUIT_COMMAND = "quit";
 const MyString& EXIT_COMMAND = "exit";
 const MyString& WHOAMI_COMMAND = "whoami";
 const MyString& ABOUT_COMMAND = "about";
-
-const int buffSize = 1024;
